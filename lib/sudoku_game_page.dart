c03c7abe278b278ffd58c9ee72f2aecf3a7689b3
João Gabriel Lima Marinho <jg.limamarinho202@gmail.com>
2024-11-11 20:19:21 -0300

    Refactor Sudoku game logic and enhance UI with victory check and error highlighting

--- lib/sudoku_game_page.dart
+++ lib/sudoku_game_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sudoku/main.dart';
 import 'package:sudoku_dart/sudoku_dart.dart';
 import 'package:tuple/tuple.dart';
 
@@ -33,8 +34,10 @@ class _SudokuGamePageState extends State<SudokuGamePage> {
   late Level level;
 
   late List<List<int>> matrix;
+  late List<List<int>> matrizSolucao;
   late List<Tuple2<int, int>> vazios;
   late List<int> quadradoSelecionado;
+  ValueNotifier<int> dialogTrigger = ValueNotifier(0);
 
   List<List<int>> changeMatrix(int value) {
     int pos1 = quadradoSelecionado[0];
@@ -46,6 +49,81 @@ class _SudokuGamePageState extends State<SudokuGamePage> {
     return matrix;
   }
 
+  bool verificarErro(int quadrante, int numero) {
+    // verificando quadrante
+    for (int i = 0; i < 9; i++) {
+      if (matrix[quadrante][i] == matrix[quadrante][numero] &&
+          i != numero &&
+          matrix[quadrante][i] != -1) {
+        return true;
+      }
+    }
+
+    // verificando linha
+    int numeroAtual = matrix[quadrante][numero];
+    int inicioDaLinha = (quadrante ~/ 3) * 3;
+    int linhaDoQuadrante = (numero ~/ 3) * 3;
+
+    for (int i = inicioDaLinha; i < inicioDaLinha + 3; i++) {
+      if (numeroAtual == -1) continue;
+      for (int j = linhaDoQuadrante; j < linhaDoQuadrante + 3; j++) {
+        if (matrix[i][j] == matrix[quadrante][numero] &&
+            (Tuple2(i, j) != Tuple2(quadrante, numero)) &&
+            matrix[i][j] != -1) {
+          return true;
+        }
+      }
+    }
+
+    int coluna = (quadrante % 3) * 3 + (numero % 3);
+    int linha = (quadrante ~/ 3) * 3 + (numero ~/ 3);
+    for (int r = 0; r < 9; r++) {
+      if (matrix[r ~/ 3 * 3 + coluna ~/ 3][coluna % 3 + r % 3 * 3] ==
+              matrix[quadrante][numero] &&
+          r != linha &&
+          matrix[r ~/ 3 * 3 + coluna ~/ 3][coluna % 3 + r % 3 * 3] != -1) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  Color coresSudoku(int quadrante, int numero) {
+    if (quadradoSelecionado[0] == quadrante &&
+        quadradoSelecionado[1] == numero) {
+      if (verificarErro(quadrante, numero)) {
+        return Colors.red.shade800;
+      }
+
+      return Colors.pink.shade100;
+    }
+
+    if (vazios.contains(Tuple2(quadrante, numero))) {
+      if (verificarErro(quadrante, numero)) {
+        return Colors.red;
+      }
+      return Colors.white;
+    } else {
+      if (verificarErro(quadrante, numero)) {
+        return Colors.red.shade300;
+      }
+
+      return Colors.grey.shade200;
+    }
+  }
+
+  bool verificarVitoria() {
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        if (matrix[i][j] != matrizSolucao[i][j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
   @override
   void initState() {
     quadradoSelecionado = [-1, -1];
@@ -53,8 +131,28 @@ class _SudokuGamePageState extends State<SudokuGamePage> {
     level = getLevel();
     Sudoku sudoku = Sudoku.generate(level);
 
-    matrix = List.generate(
-        9, (i) => List.generate(9, (j) => sudoku.puzzle[i * 9 + j]));
+    List<List<int>> indices = [
+      [0, 1, 2, 9, 10, 11, 18, 19, 20],
+      [3, 4, 5, 12, 13, 14, 21, 22, 23],
+      [6, 7, 8, 15, 16, 17, 24, 25, 26],
+      [27, 28, 29, 36, 37, 38, 45, 46, 47],
+      [30, 31, 32, 39, 40, 41, 48, 49, 50],
+      [33, 34, 35, 42, 43, 44, 51, 52, 53],
+      [54, 55, 56, 63, 64, 65, 72, 73, 74],
+      [57, 58, 59, 66, 67, 68, 75, 76, 77],
+      [60, 61, 62, 69, 70, 71, 78, 79, 80],
+    ];
+    matrix = [];
+    matrizSolucao = [];
+    for (var indice in indices) {
+      List<int> sublista = indice.map((i) => sudoku.puzzle[i]).toList();
+      matrix.add(sublista);
+    }
+    for (var indice in indices) {
+      List<int> sublista = indice.map((i) => sudoku.solution[i]).toList();
+      matrizSolucao.add(sublista);
+    }
+
     vazios = [];
     for (int i = 0; i < 9; i++) {
       for (int j = 0; j < 9; j++) {
@@ -158,14 +256,7 @@ class _SudokuGamePageState extends State<SudokuGamePage> {
                                     })
                                   },
                                   child: Container(
-                                    color:
-                                        quadradoSelecionado[0] == quadrante &&
-                                                quadradoSelecionado[1] == numero
-                                            ? Colors.pink.shade100
-                                            : vazios.contains(
-                                                    Tuple2(quadrante, numero))
-                                                ? Colors.white
-                                                : Colors.grey.shade200,
+                                    color: coresSudoku(quadrante, numero),
                                     alignment: Alignment.center,
                                     child: Text(
                                       matrix[quadrante][numero] == -1
@@ -189,8 +280,8 @@ class _SudokuGamePageState extends State<SudokuGamePage> {
                 direction: Axis.horizontal,
                 alignment: WrapAlignment.center,
                 runAlignment: WrapAlignment.center,
-                spacing: 10,
-                runSpacing: 10,
+                spacing: 5,
+                runSpacing: 5,
                 children: [
                   FilledButton(
                       onPressed: () {
@@ -263,7 +354,43 @@ class _SudokuGamePageState extends State<SudokuGamePage> {
                 ],
               ),
               SizedBox(height: 20),
-              ElevatedButton(onPressed: () {}, child: Text("Novo Jogo")),
+              Wrap(
+                spacing: 10,
+                runSpacing: 10,
+                alignment: WrapAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context, rootNavigator: true).pop(context);
+                      },
+                      child: Text("Novo Jogo")),
+                  FilledButton(
+                    onPressed: !verificarVitoria()
+                        ? null
+                        : () => showDialog<String>(
+                              context: context,
+                              builder: (BuildContext context) => AlertDialog(
+                                title: const Text('Parabéns!'),
+                                content: const Text('Você venceu o jogo!'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.pop(context, 'Cancel'),
+                                    child: const Text('Fechar'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () => Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                            builder: (context) => RootPage())),
+                                    child: const Text('Novo Jogo'),
+                                  ),
+                                ],
+                              ),
+                            ),
+                    child: const Text('Verificar'),
+                  )
+                ],
+              ),
             ],
           ),
         ));
